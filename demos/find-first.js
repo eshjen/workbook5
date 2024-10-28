f56c114ddfd6b8fed120867a10429787dbe15076
Eshei Jenkins <eshjenk@gmail.com>
2024-10-28 10:25:17 -0400

    added some comments

--- demos/find-first.js
+++ demos/find-first.js
@@ -1,7 +1,7 @@
 "use strict";
 
 let numbers =  [7, 9, 64, 60, 12, 13, 65, 62];
-// 
+
 function isOver(arrayValue){
 if(arrayValue > 60){
     return true;
@@ -9,5 +9,6 @@ if(arrayValue > 60){
     return false;
 }
 }
-let firstValueOver60 = numbers.find(isOver60);
+let firstValueOver60 = numbers.find(isOver60); // this function is finding the FIRST value that is over 60.
 console.log(firstValueOver60);
+// if youre trying to find the FIRST thing, or ONE thing, use .find
