a33879760c8297f8c52efc26c7583fa9f920104b
Eshei Jenkins <eshjenk@gmail.com>
2024-10-29 09:37:26 -0400

    basic arrow function

---
+++ demos/arrow-function.js
@@ -0,0 +1,28 @@
+"use strict";
+//defining a function
+// function add(number1, number2) {
+//     //number1, number2 are parameters
+//     let result = number1 + number2;
+//     return result;
+//   }
+  // 1. function keyword
+  // 2. function name
+  // 3. parenthesis() hold the parameters (input variables) separated by commas
+  // 4. curly braces are a code block and enclose the function body (the steps)
+  
+  let add = (number1, number2) => {
+    //number1, number2 are parameters
+    let result = number1 + number2;
+    return result;
+
+// 
+
+
+
+
+
+  //calling a function
+  let output = add(2, 2); //function name followed by parenthesis
+  //          <-------
+  //when called the function returns its output into the variable on the left
+  console.log(output); //4
