9cc7c181e3f1c6b4387111be6b95b98824be8b48
Eshei Jenkins <eshjenk@gmail.com>
2024-10-30 11:55:07 -0400

    sorting in ascending order

--- demos/array-sort-numbers.js
+++ demos/array-sort-numbers.js
@@ -1,4 +1,10 @@
 "use strict";
 const randomNumbers = [298, 754, 855, 413, 579, 932, 533, 64, 464, 344, 65, 636, 439, 903, 606, 205, 971, 743, 623];
-randomNumbers.sort();
+
+randomNumbers.sort(function (a, b) {
+  if (a < b) return -1; //right order
+  else if (a == b) return 0; //same values
+  else return 1; //swap, wrong order
+});
+
 console.log(randomNumbers);
