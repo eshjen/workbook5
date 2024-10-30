09187953082c1c98db0346ed4e6557e7e11c383b
Eshei Jenkins <eshjenk@gmail.com>
2024-10-30 11:52:00 -0400

    soting numbers

---
+++ demos/array-sort-numbers.js
@@ -0,0 +1,4 @@
+"use strict";
+const randomNumbers = [298, 754, 855, 413, 579, 932, 533, 64, 464, 344, 65, 636, 439, 903, 606, 205, 971, 743, 623];
+randomNumbers.sort();
+console.log(randomNumbers);
